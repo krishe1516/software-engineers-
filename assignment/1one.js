a13151c964aa1ef6cc74ad70b758c0cc4224d7c9
shinde0702 <abhijeetshinde596@gmail.com>
2022-11-27 17:37:40 +0530

    save file

--- assignment/1one.js
+++ assignment/1one.js
@@ -1,5 +1,3 @@
-// 1. What is the value of y after evaluating the expression given below?    y+=++y + y-- + --y; when let y=8
-
 let y=8;
 
 y+=++y+y--+--y;
--- assignment/5five.js
+++ assignment/5five.js
@@ -1,13 +1,11 @@
 /*(   " < "   ) It is Relational type Operator it defined Less than oprand 
-              ex --> 40<50  here 40 is less than 50
 
 
 
-  (   " ++ "   ) It is  Unary type Operator it shows increment in oprand. 
-                 ex --> 7++ = 8
+
+  (   " ++ "   ) It is  Unary type Operator it shows increment in oprand
     
   (   " && "   ) It is logical type Operatorc it is use to check  one or more condition
 
-  Ex  - math>=35  && marathi>=35 
 
   (   " ? "   )  It is use for checking condition*/
